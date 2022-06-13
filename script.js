a3b62ca5e73dae5398e7cecb341ba81eca85416a
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-06-13 07:43:07 +0700

    try add data to databse

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const User = require("./User");
+mongoose.connect(
+  "mongodb://localhost/fundamental",
+  () => {
+    console.log("connect to mongoose");
+  },
+  (e) => {
+    console.log(`Error:  ${e}`);
+  }
+);
 
-mongoose.connect("mongodb://localhost/fundamental",()=>{
-    console.log("connect to mongoose")
-},(e)=>{
-    console.log(`Error:  ${e}`)
-})
+const user = new User({ name: "Siska", age: 23 });
+user.save().then(() => console.log("user save"));
